e0ab6d25387f893c659391b24ffdfaa91352dfb6
bk dev <fox.physics@outlook.fr>
2020-11-06 04:17:43 +0100

    Updates

---
+++ precache-manifest.cbd197d8e091f68f8104acb36d8b33c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc6e9c91be0e4f732458c62e2b79624",
+    "url": "/bk-netflix-clone/index.html"
+  },
+  {
+    "revision": "1df6ae6e6fb20313271a",
+    "url": "/bk-netflix-clone/static/css/main.29891145.chunk.css"
+  },
+  {
+    "revision": "88d7e49efa8a81eace14",
+    "url": "/bk-netflix-clone/static/js/2.965917ba.chunk.js"
+  },
+  {
+    "revision": "8c531855ea370064c601b9c584684a5a",
+    "url": "/bk-netflix-clone/static/js/2.965917ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1df6ae6e6fb20313271a",
+    "url": "/bk-netflix-clone/static/js/main.86fb7037.chunk.js"
+  },
+  {
+    "revision": "5857ad3bfc7628f4c686",
+    "url": "/bk-netflix-clone/static/js/runtime-main.c4cce0c7.js"
+  }
+]);
